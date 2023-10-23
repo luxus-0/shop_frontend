6aaadb5e6eef3282703773afd626b63925482f25
Łukasz <luxus0@gmail.com>
2023-10-23 21:12:12 +0200

    Add Fullpage module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProductComponent } from './modules/product/product.component';
+import { FullpageComponent } from './layouts/fullpage/fullpage.component';
+import { LoginComponent } from './modules/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: DefaultComponent, children: [
+      { path: '', component : HomeComponent },
+      { path: 'products', component : ProductComponent },
+    ]
+  },
+  {
+    path:'', component: FullpageComponent, children: [
+      { path: '', component : LoginComponent },
+      { path: 'login', component : ProductComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { FullpageModule } from './layouts/fullpage/fullpage.module';
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DefaultModule,
+        FullpageModule,
+        SharedModule
+    ],
+    exports:[
+      SharedModule
+    ]
 })
 export class AppModule { }
